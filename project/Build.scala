ba26283449bedc76ac4a02877592d71fa07a1fe4
Helena Hjertén <helena@hjerten.com>
2012-04-23 19:57:52 +0200

    Commit and push to github

--- project/Build.scala
+++ project/Build.scala
@@ -4,7 +4,7 @@ import PlayProject._
 
 object ApplicationBuild extends Build {
 
-    val appName         = "sp2012"
+    val appName         = "sp2012-prebaked"
     val appVersion      = "1.0-SNAPSHOT"
 
     val appDependencies = Seq(
